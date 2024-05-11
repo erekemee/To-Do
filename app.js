3a683c2f0ad90f8d6b4b4d966eafa55456ab1b96
erekemee <167605768+erekemee@users.noreply.github.com>
2024-05-11 16:34:58 +0800

    :)
    :)

---
+++ app.js
@@ -0,0 +1,40 @@
+var ol = document.getElementsByTagName('ol')[0]
+const input = document.getElementsByTagName('input')[0]
+const addBtn = document.getElementsByTagName('button')[0];
+
+addBtn.addEventListener("click",()=>{
+	let  li = document.createElement('li');
+	console.log(li);
+	li.innerText = input.value;
+	ol.append(li);
+	input.value="";
+	let delBtn = document.createElement('button');
+	delBtn.innerHTML = '<i class="bi bi-trash3"></i>' ;
+	li.append(delBtn);
+	let checkBtn = document.createElement('button');
+	checkBtn.innerHTML = '<i class="bi bi-clipboard-check"></i>';
+	li.append(checkBtn);
+	delBtn.className = "del";
+	checkBtn.classList.toggle=('<i class="bi bi-clipboard2-minus"></i>');
+	checkBtn.innerHTML = checkBtn.classList.toggle
+	saveData();
+})
+
+ol.addEventListener("click",(e)=>{
+	console.log(e.target);
+	let targetEl = e.target;
+	let parentEl = (targetEl.parentElement).parentElement;
+	if(e.target.className=="bi bi-trash3"){
+		parentEl.remove();
+	}else if(e.target.className=="bi bi-clipboard-check"){
+		parentEl.classList.toggle("checked");
+	}
+	saveData();
+});
+function saveData(){
+         localStorage.setItem("e",ol.innerHTML);
+}
+function getData(){
+         ol.innerHTML = localStorage.getItem("e");
+}
+getData();
